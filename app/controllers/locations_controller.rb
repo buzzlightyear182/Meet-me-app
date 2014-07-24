9d79ea427f09d0948350a6515314244727c229ba
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-07-24 14:31:10 +0200

    Added create location and save function

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,37 @@
 class LocationsController < ApplicationController
 
 	def index
+		@locations = Location.all
 		# @locations = Location.last_created(7)
-		@locations = Location.in_spain?
+		# @locations = Location.in_spain?
 	end
 
 	def show
 		@location = Location.find(params[:id])
 	rescue ActiveRecord::RecordNotFound
 		render plain: 'Sorry, not found', status: 404 unless @location
+	end
+
+	def new
+		@location = Location.new
+	end
+
+	def create
+		@location = Location.new location_params
+		@location.save
+		if @location.save
+			flash[:notice] = "Location created!"
+			redirect_to action:'index', controller:'locations'
+		else
+			@errors = @location.errors.full_messages
+			render 'new'
+		end
+	end
+
+private
+
+	def location_params
+		params.require(:location).permit(:name, :city, :country)
 	end
 
 end
