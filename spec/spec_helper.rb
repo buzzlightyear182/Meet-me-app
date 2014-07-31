2ad27e29942c91376e602d8eef10d197a105030f
benja83 <tarennebenjamin@gmail.com>
2014-07-31 17:18:09 +0200

    added scenario test Signing in with correct credentials

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,21 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+require 'capybara/rspec'
+require 'capybara/rails'
+
 RSpec.configure do |config|
+  
+  config.include Rails.application.routes.url_helpers
+  config.include Devise::TestHelpers, :type => :controller
+  # config.include Devise::SpecHelpers, :type => :controller
+  # config.include HelperMethods, :type => :request
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
